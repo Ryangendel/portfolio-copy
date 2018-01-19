4ef705f162e17ab4d27edafe6cbb46edbe97cbf1
ryan gendel <ryangendel@ryans-MacBook-Pro.local>
2018-01-18 22:49:17 -0700

    car

---
+++ app/routing/htmlRoutes.js
@@ -0,0 +1,43 @@
+// ===============================================================================
+// DEPENDENCIES
+// We need to include the path package to get the correct file path for our html
+// ===============================================================================
+var path = require("path");
+
+// ===============================================================================
+// ROUTING
+// ===============================================================================
+
+module.exports = function(app) {
+
+  // HTML GET Requests
+  // Below code handles when users "visit" a page.
+  // In each of the below cases the user is shown an HTML page of content
+  // ---------------------------------------------------------------------------
+
+  app.get("/", function(req, res) {
+    res.sendFile(path.join(__dirname, "/../public/index.html"));
+  });
+
+  // If no matching route is found default to home
+  app.get("/design", function(req, res) {
+    res.sendFile(path.join(__dirname, "/../public/design.html"));
+  });
+
+  app.get("/coding", function(req, res) {
+    res.sendFile(path.join(__dirname, "/../public/coding.html"));
+  });
+
+  app.get("/analytics", function(req, res) {
+    res.sendFile(path.join(__dirname, "/../public/analytics.html"));
+  });
+
+  app.get("/contact", function(req, res) {
+    res.sendFile(path.join(__dirname, "/../public/contact.html"));
+  });
+
+  app.get("/demoreel", function(req, res) {
+    res.sendFile(path.join(__dirname, "/../public/demoreel.html"));
+  });
+
+};
